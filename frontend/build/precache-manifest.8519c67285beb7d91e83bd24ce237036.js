03e48eb6f6dcb6628854f754f9017c1095d36e72
Atharv Patil <71587540+Atharvp18@users.noreply.github.com>
2023-11-03 13:54:12 +0400

    Add files via upload

---
+++ frontend/build/precache-manifest.8519c67285beb7d91e83bd24ce237036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04850240b0b2af3b0b0ed189cbd07dac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebd36ddd9f1129d9131f",
+    "url": "./static/css/main.2e95961f.chunk.css"
+  },
+  {
+    "revision": "5c3db440e55bc953e812",
+    "url": "./static/js/2.09ad65c5.chunk.js"
+  },
+  {
+    "revision": "ebd36ddd9f1129d9131f",
+    "url": "./static/js/main.6623d341.chunk.js"
+  },
+  {
+    "revision": "09956ca49300959daff9",
+    "url": "./static/js/runtime-main.ac4bc109.js"
+  }
+]);
