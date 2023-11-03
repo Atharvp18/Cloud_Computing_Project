237f8ac2f5b621f9e9e8d99b723cbeec403a060e
Atharv Patil <71587540+Atharvp18@users.noreply.github.com>
2023-11-03 13:19:37 +0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
+const path = require('path')
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../frontend/build");
 
 const app = express();
+// app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        // "../frontend/build/index.html",
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
 
 app.use(bodyParser.json());
 
@@ -37,14 +63,28 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
+const PORT = 5000;
 mongoose
-  .connect(
-    `mongodb+srv://atharvp18:atharvp18@cluster0.2pcnjfv.mongodb.net/places?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
+  .connect('mongodb+srv://atharvp18:atharvp1801@cluster0.2pcnjfv.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => {
-    app.listen(5000);
+    console.log(`Connected to Database`);
   })
   .catch((err) => {
     console.log(err);
   });
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://atharvp18:atharvp1801@cluster0.2pcnjfv.mongodb.net/?retryWrites=true&w=majority",
+//     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+//   )
+//   .then(() => {
+//     app.listen(PORT);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
